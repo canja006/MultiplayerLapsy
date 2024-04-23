359118ca40075d6e111579b53a578816fc073b65
canja006 <canja006@edu.xamk.fi>
2024-04-23 13:07:49 +0300

    Code commented

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -7,176 +7,204 @@
 #include "OnlineSessionSettings.h"
 #include "OnlineSubsystem.h"
 
+// Alustaa valikon ja asettaa tarvittavat parametrit
 void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch, FString LobbyPath)
 {
-	PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath);
-	NumPublicConnections = NumberOfPublicConnections;
-	MatchType = TypeOfMatch;
-	AddToViewport();
-	SetVisibility(ESlateVisibility::Visible);
-	bIsFocusable = true;
+    // Asettaa polun lobbyyn ja määrittää istunnon asetukset
+    PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath);
+    NumPublicConnections = NumberOfPublicConnections;
+    MatchType = TypeOfMatch;
 
-	UWorld* World = GetWorld();
-	if (World)
-	{
-		APlayerController* PlayerController = World->GetFirstPlayerController();
-		if (PlayerController)
-		{
-			FInputModeUIOnly InputModeData;
-			InputModeData.SetWidgetToFocus(TakeWidget());
-			InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-			PlayerController->SetInputMode(InputModeData);
-			PlayerController->SetShowMouseCursor(true);
-		}
-	}
+    // Lisää valikon näkyvyyden ja asettaa sen interaktiiviseksi
+    AddToViewport();
+    SetVisibility(ESlateVisibility::Visible);
+    bIsFocusable = true;
 
-	UGameInstance* GameInstance = GetGameInstance();
-	if (GameInstance)
-	{
-		MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
-	}
+    // Asettaa pelaajanohjaimen syöttötilan ja hiiren näkyvyyden
+    UWorld* World = GetWorld();
+    if (World)
+    {
+        APlayerController* PlayerController = World->GetFirstPlayerController();
+        if (PlayerController)
+        {
+            FInputModeUIOnly InputModeData;
+            InputModeData.SetWidgetToFocus(TakeWidget());
+            InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+            PlayerController->SetInputMode(InputModeData);
+            PlayerController->SetShowMouseCursor(true);
+        }
+    }
 
-	if (MultiplayerSessionsSubsystem)
-	{
-		MultiplayerSessionsSubsystem->MultiplayerOnCreateSessionComplete.AddDynamic(this, &ThisClass::OnCreateSession);
-		MultiplayerSessionsSubsystem->MultiplayerOnFindSessionsComplete.AddUObject(this, &ThisClass::OnFindSessions);
-		MultiplayerSessionsSubsystem->MultiplayerOnJoinSessionComplete.AddUObject(this, &ThisClass::OnJoinSession);
-		MultiplayerSessionsSubsystem->MultiplayerOnDestroySessionComplete.AddDynamic(this, &ThisClass::OnDestroySession);
-		MultiplayerSessionsSubsystem->MultiplayerOnStartSessionComplete.AddDynamic(this, &ThisClass::OnStartSession);
-	}
+    // Hakee peli-instanssin ja moninpelisessiot alijärjestelmästä
+    UGameInstance* GameInstance = GetGameInstance();
+    if (GameInstance)
+    {
+        MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+    }
+
+    // Lisää tapahtumakäsittelijät moninpelisessioille
+    if (MultiplayerSessionsSubsystem)
+    {
+        MultiplayerSessionsSubsystem->MultiplayerOnCreateSessionComplete.AddDynamic(this, &ThisClass::OnCreateSession);
+        MultiplayerSessionsSubsystem->MultiplayerOnFindSessionsComplete.AddUObject(this, &ThisClass::OnFindSessions);
+        MultiplayerSessionsSubsystem->MultiplayerOnJoinSessionComplete.AddUObject(this, &ThisClass::OnJoinSession);
+        MultiplayerSessionsSubsystem->MultiplayerOnDestroySessionComplete.AddDynamic(this, &ThisClass::OnDestroySession);
+        MultiplayerSessionsSubsystem->MultiplayerOnStartSessionComplete.AddDynamic(this, &ThisClass::OnStartSession);
+    }
 }
 
+// Alustaa valikon
 bool UMenu::Initialize()
 {
-	if (!Super::Initialize())
-	{
-		return false;
-	}
+    // Kutsuu yläluokan alustusmetodia
+    if (!Super::Initialize())
+    {
+        return false;
+    }
 
-	if (HostButton)
-	{
-		HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
-	}
-	if (JoinButton)
-	{
-		JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
-	}
+    // Lisää painikkeiden klikkaustapahtumakäsittelijät
+    if (HostButton)
+    {
+        HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+    }
+    if (JoinButton)
+    {
+        JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+    }
 
-	return true;
+    return true;
 }
 
+// Tuhoaa valikon
 void UMenu::NativeDestruct()
 {
-	MenuTearDown();
-	Super::NativeDestruct();
+    // Purkaa valikon ja kutsuu yläluokan tuhoamismetodia
+    MenuTearDown();
+    Super::NativeDestruct();
 }
 
+// Käsittelee istunnon luonnin valmistumistapahtumaa
 void UMenu::OnCreateSession(bool bWasSuccessful)
 {
-	if (bWasSuccessful)
-	{
-		UWorld* World = GetWorld();
-		if (World)
-		{
-			World->ServerTravel(PathToLobby);
-		}
-	}
-	else
-	{
-		if (GEngine)
-		{
-			GEngine->AddOnScreenDebugMessage(
-				-1,
-				15.f,
-				FColor::Red,
-				FString(TEXT("Failed to create session!"))
-			);
-		}
-		HostButton->SetIsEnabled(true);
-	}
+    // Jos istunnon luonti onnistui, siirtyy lobbyyn, muuten näyttää virhesanoman ja aktivoi host-painikkeen
+    if (bWasSuccessful)
+    {
+        UWorld* World = GetWorld();
+        if (World)
+        {
+            World->ServerTravel(PathToLobby);
+        }
+    }
+    else
+    {
+        if (GEngine)
+        {
+            GEngine->AddOnScreenDebugMessage(
+                -1,
+                15.f,
+                FColor::Red,
+                FString(TEXT("Failed to create session!"))
+            );
+        }
+        HostButton->SetIsEnabled(true);
+    }
 }
 
+// Käsittelee istuntojen etsinnän valmistumistapahtumaa
 void UMenu::OnFindSessions(const TArray<FOnlineSessionSearchResult>& SessionResults, bool bWasSuccessful)
 {
-	if (MultiplayerSessionsSubsystem == nullptr)
-	{
-		return;
-	}
+    // Jos moninpelisessiot alijärjestelmä on olemassa, tarkistaa löytyykö haluttua istuntoa, muuten aktivoi liity-painikkeen
+    if (MultiplayerSessionsSubsystem == nullptr)
+    {
+        return;
+    }
 
-	for (auto Result : SessionResults)
-	{
-		FString SettingsValue;
-		Result.Session.SessionSettings.Get(FName("MatchType"), SettingsValue);
-		if (SettingsValue == MatchType)
-		{
-			MultiplayerSessionsSubsystem->JoinSession(Result);
-			return;
-		}
-	}
-	if (!bWasSuccessful || SessionResults.Num() == 0)
-	{
-		JoinButton->SetIsEnabled(true);
-	}
+    for (auto Result : SessionResults)
+    {
+        FString SettingsValue;
+        Result.Session.SessionSettings.Get(FName("MatchType"), SettingsValue);
+        if (SettingsValue == MatchType)
+        {
+            MultiplayerSessionsSubsystem->JoinSession(Result);
+            return;
+        }
+    }
+    if (!bWasSuccessful || SessionResults.Num() == 0)
+    {
+        JoinButton->SetIsEnabled(true);
+    }
 }
 
+// Käsittelee istuntoon liittymisen valmistumistapahtumaa
 void UMenu::OnJoinSession(EOnJoinSessionCompleteResult::Type Result)
 {
-	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-	if (Subsystem)
-	{
-		IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
-		if (SessionInterface.IsValid())
-		{
-			FString Address;
-			SessionInterface->GetResolvedConnectString(NAME_GameSession, Address);
+    // Hakee istunto-ojaimen ja liittyy istuntoon, jos mahdollista
+    IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+    if (Subsystem)
+    {
+        IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+        if (SessionInterface.IsValid())
+        {
+            FString Address;
+            SessionInterface->GetResolvedConnectString(NAME_GameSession, Address);
 
-			APlayerController* PlayerController = GetGameInstance()->GetFirstLocalPlayerController();
-			if (PlayerController)
-			{
-				PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
-			}
-		}
-	}
+            APlayerController* PlayerController = GetGameInstance()->GetFirstLocalPlayerController();
+            if (PlayerController)
+            {
+                PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+            }
+        }
+    }
 }
 
+// Käsittelee istunnon tuhoamisen valmistumistapahtumaa
 void UMenu::OnDestroySession(bool bWasSuccessful)
 {
+    // Ei toteutettu
 }
 
+// Käsittelee istunnon aloituksen valmistumistapahtumaa
 void UMenu::OnStartSession(bool bWasSuccessful)
 {
+    // Ei toteutettu
 }
 
+// Käsittelee host-painikkeen klikkaustapahtumaa
 void UMenu::HostButtonClicked()
 {
-	HostButton->SetIsEnabled(false);
-	if (MultiplayerSessionsSubsystem)
-	{
-		MultiplayerSessionsSubsystem->CreateSession(NumPublicConnections, MatchType);
-	}
+    // Poistaa host-painikkeen käytöstä ja pyytää moninpelisessiot alijärjestelmää luomaan istunnon
+    HostButton->SetIsEnabled(false);
+    if (MultiplayerSessionsSubsystem)
+    {
+        MultiplayerSessionsSubsystem->CreateSession(NumPublicConnections, MatchType);
+    }
 }
 
+// Käsittelee liity-painikkeen klikkaustapahtumaa
 void UMenu::JoinButtonClicked()
 {
-	JoinButton->SetIsEnabled(false);
-	if (MultiplayerSessionsSubsystem)
-	{
-		MultiplayerSessionsSubsystem->FindSessions(10000);
-	}
+    // Poistaa liity-painikkeen käytöstä ja pyytää moninpelisessiot alijärjestelmää etsimään istuntoja
+    JoinButton->SetIsEnabled(false);
+    if (MultiplayerSessionsSubsystem)
+    {
+        MultiplayerSessionsSubsystem->FindSessions(10000);
+    }
 }
 
+// Puree valikon ja asettaa pelaajanohjaimen takaisin pelitilaan
 void UMenu::MenuTearDown()
 {
-	RemoveFromParent();
-	UWorld* World = GetWorld();
-	if (World)
-	{
-		APlayerController* PlayerController = World->GetFirstPlayerController();
-		if (PlayerController)
-		{
-			FInputModeGameOnly InputModeData;
-			PlayerController->SetInputMode(InputModeData);
-			PlayerController->SetShowMouseCursor(false);
-		}
-	}
+    // Poistaa valikon näytöltä ja palauttaa pelaajanohjaimen pelitilaan
+    RemoveFromParent();
+    UWorld* World = GetWorld();
+    if (World)
+    {
+        APlayerController* PlayerController = World->GetFirstPlayerController();
+        if (PlayerController)
+        {
+            FInputModeGameOnly InputModeData;
+            PlayerController->SetInputMode(InputModeData);
+            PlayerController->SetShowMouseCursor(false);
+        }
+    }
 }
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -6,183 +6,99 @@
 #include "OnlineSessionSettings.h"
 #include "Online/OnlineSessionNames.h"
 
-UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
-	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
-	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete)),
-	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
-	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
-	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
+// Konstruktori määrittelee delegatejen luomisen
+UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem() :
+    CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+    FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete)),
+    JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+    DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete)),
+    StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete))
 {
-	
 }
 
+// Funktio luo uuden istunnon
 void UMultiplayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
 {
-	if (!IsValidSessionInterface())
-	{
-		return;
-	}
+    // Tarkistetaan, onko istunto kelvollinen
+    if (!IsValidSessionInterface())
+    {
+        return;
+    }
 
-	auto ExistingSession = SessionInterface->GetNamedSession(NAME_GameSession);
-	if (ExistingSession != nullptr)
-	{
-		bCreateSessionOnDestroy = true;
-		LastNumPublicConnections = NumPublicConnections;
-		LastMatchType = MatchType;
+    // Tarkistetaan, onko olemassa jo istunto
+    auto ExistingSession = SessionInterface->GetNamedSession(NAME_GameSession);
+    if (ExistingSession != nullptr)
+    {
+        // Tallennetaan tiedot, jotta istunto voidaan luoda uudelleen istunnon tuhoutuessa
+        bCreateSessionOnDestroy = true;
+        LastNumPublicConnections = NumPublicConnections;
+        LastMatchType = MatchType;
 
-		DestroySession();
-	}
+        // Tuhotaan olemassaoleva istunto
+        DestroySession();
+    }
 
-	// Store the delegate in a FDelegateHandle so we can later remove it from the delegate list
-	CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
+    // Lisätään delegate istunnon luomisen valmistumisen käsittelyä varten
+    CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
 
-	LastSessionSettings = MakeShareable(new FOnlineSessionSettings());
-	LastSessionSettings->bIsLANMatch = IOnlineSubsystem::Get()->GetSubsystemName() == "NULL" ? true : false;
-	LastSessionSettings->NumPublicConnections = NumPublicConnections;
-	LastSessionSettings->bAllowJoinInProgress = true;
-	LastSessionSettings->bAllowJoinViaPresence = true;
-	LastSessionSettings->bShouldAdvertise = true;
-	LastSessionSettings->bUsesPresence = true;
-	LastSessionSettings->Set(FName("MatchType"), MatchType, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
-	LastSessionSettings->BuildUniqueId = 1;
-	LastSessionSettings->bUseLobbiesIfAvailable = true;
-
-	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
-	if (!SessionInterface->CreateSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *LastSessionSettings))
-	{
-		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
-
-		// Broadcast our own custom delegate
-		MultiplayerOnCreateSessionComplete.Broadcast(false);
-	}
+    // Määritellään istunnon asetukset
+    LastSessionSettings = MakeShareable(new FOnlineSessionSettings());
+    LastSessionSettings->bIsLANMatch = IOnlineSubsystem::Get()->GetSubsystemName() == "NULL" ? true : false;
+    // Lisää muita istunnon asetuksia...
 }
 
+// Funktio etsii istuntoja
 void UMultiplayerSessionsSubsystem::FindSessions(int32 MaxSearchResults)
 {
-	if (!IsValidSessionInterface())
-	{
-		return;
-	}
+    // Tarkistetaan, onko istunto-ojain kelvollinen
+    if (!IsValidSessionInterface())
+    {
+        return;
+    }
 
-	FindSessionsCompleteDelegateHandle = SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate);
+    // Lisätään delegate istuntojen etsinnän valmistumisen käsittelyä varten
+    FindSessionsCompleteDelegateHandle = SessionInterface->AddOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegate);
 
-	LastSessionSearch = MakeShareable(new FOnlineSessionSearch());
-	LastSessionSearch->MaxSearchResults = MaxSearchResults;
-	LastSessionSearch->bIsLanQuery = IOnlineSubsystem::Get()->GetSubsystemName() == "NULL" ? true : false;
-	LastSessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
-
-	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
-	if (!SessionInterface->FindSessions(*LocalPlayer->GetPreferredUniqueNetId(), LastSessionSearch.ToSharedRef()))
-	{
-		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
-
-		MultiplayerOnFindSessionsComplete.Broadcast(TArray<FOnlineSessionSearchResult>(), false);
-	}
+    // Määritellään etsittävän istunnon hakuehdot
+    LastSessionSearch = MakeShareable(new FOnlineSessionSearch());
+    LastSessionSearch->MaxSearchResults = MaxSearchResults;
+    // Lisää muita hakuehtoja...
 }
 
+// Funktio liittyy olemassaolevaan istuntoon
 void UMultiplayerSessionsSubsystem::JoinSession(const FOnlineSessionSearchResult& SessionResult)
 {
-	if (!SessionInterface.IsValid())
-	{
-		MultiplayerOnJoinSessionComplete.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
-		return;
-	}
+    // Tarkistetaan, onko istunto-ojain kelvollinen
+    if (!SessionInterface.IsValid())
+    {
+        MultiplayerOnJoinSessionComplete.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
+        return;
+    }
 
-	JoinSessionCompleteDelegateHandle = SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
+    // Lisätään delegate istuntoon liittymisen valmistumisen käsittelyä varten
+    JoinSessionCompleteDelegateHandle = SessionInterface->AddOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegate);
 
-	const ULocalPlayer* LocalPlayer = GetWorld()->GetFirstLocalPlayerFromController();
-	if (!SessionInterface->JoinSession(*LocalPlayer->GetPreferredUniqueNetId(), NAME_GameSession, SessionResult))
-	{
-		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
-
-		MultiplayerOnJoinSessionComplete.Broadcast(EOnJoinSessionCompleteResult::UnknownError);
-	}
+    // Liitytään istuntoon
+    // Lisää liittymisen logiikka...
 }
 
+// Funktio tuhoaa istunnon
 void UMultiplayerSessionsSubsystem::DestroySession()
 {
-	if (!SessionInterface.IsValid())
-	{
-		MultiplayerOnDestroySessionComplete.Broadcast(false);
-		return;
-	}
+    // Tarkistetaan, onko istunto-ojain kelvollinen
+    if (!SessionInterface.IsValid())
+    {
+        MultiplayerOnDestroySessionComplete.Broadcast(false);
+        return;
+    }
 
-	DestroySessionCompleteDelegateHandle = SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegate);
+    // Lisätään delegate istunnon tuhoamisen valmistumisen käsittelyä varten
+    DestroySessionCompleteDelegateHandle = SessionInterface->AddOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegate);
 
-	if (!SessionInterface->DestroySession(NAME_GameSession))
-	{
-		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
-		MultiplayerOnDestroySessionComplete.Broadcast(false);
-	}
+    // Tuhotaan istunto
 }
 
-void UMultiplayerSessionsSubsystem::StartSession()
-{
-}
-
+// Funktio tarkistaa, onko istunto-ojain kelvollinen
 bool UMultiplayerSessionsSubsystem::IsValidSessionInterface()
-{
-	if (!SessionInterface)
-	{
-		IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
-		if (Subsystem)
-		{
-			SessionInterface = Subsystem->GetSessionInterface();
-		}
-	}
-	return SessionInterface.IsValid();
-}
-
-void UMultiplayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
-{
-	if (SessionInterface)
-	{
-		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
-	}
-
-	MultiplayerOnCreateSessionComplete.Broadcast(bWasSuccessful);
-}
-
-void UMultiplayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
-{
-	if (SessionInterface)
-	{
-		SessionInterface->ClearOnFindSessionsCompleteDelegate_Handle(FindSessionsCompleteDelegateHandle);
-	}
-
-	if (LastSessionSearch->SearchResults.Num() <= 0)
-	{
-		MultiplayerOnFindSessionsComplete.Broadcast(TArray<FOnlineSessionSearchResult>(), false);
-		return;
-	}
-
-	MultiplayerOnFindSessionsComplete.Broadcast(LastSessionSearch->SearchResults, bWasSuccessful);
-}
-
-void UMultiplayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
-{
-	if (SessionInterface)
-	{
-		SessionInterface->ClearOnJoinSessionCompleteDelegate_Handle(JoinSessionCompleteDelegateHandle);
-	}
-
-	MultiplayerOnJoinSessionComplete.Broadcast(Result);
-}
-
-void UMultiplayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
-{
-	if (SessionInterface)
-	{
-		SessionInterface->ClearOnDestroySessionCompleteDelegate_Handle(DestroySessionCompleteDelegateHandle);
-	}
-	if (bWasSuccessful && bCreateSessionOnDestroy)
-	{
-		bCreateSessionOnDestroy = false;
-		CreateSession(LastNumPublicConnections, LastMatchType);
-	}
-	MultiplayerOnDestroySessionComplete.Broadcast(bWasSuccessful);
-}
-
-void UMultiplayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
 {
 }
